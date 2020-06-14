c821a9ebb4bfeb0f859eb6f38bdd11c134e244a4
quangnguyen90 <nguyenphuquang90@gmail.com>
2020-06-14 23:45:46 +0700

    Setup UI with reactstrap

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+import { Col, Container, Row } from 'reactstrap';
+import './Header.scss';
 
-Header.propTypes = {
+Header.propTypes = {};
 
-};
-
-function Header(props) {
+function Header() {
     return (
-        <div>
+        <header className="header">
+            <Container>
+                <Row className="justify-content-between">
+                    <Col xs="auto">
+                        <a
+                            className="header__link header__title"
+                            href="http://github.com/quangnguyen90"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            My Github
+                        </a>
+                    </Col>
 
-        </div>
+                    <Col xs="auto">
+                        <NavLink
+                            exact
+                            className="header__link"
+                            to="/photos"
+                            activeClassName="header__link--active"
+                        >
+                            My Redux Photo App
+                        </NavLink>
+                    </Col>
+                </Row>
+            </Container>
+        </header>
     );
 }
 
-export default Header
+export default Header;
--- src/features/Photo/pages/Main/index.jsx
+++ src/features/Photo/pages/Main/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import Banner from '../../../../components/Banner';
+import Images from '../../../../constants/images';
+import { Container } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
 
 MainPage.propTypes = {};
 
 function MainPage(props) {
     return (
-        <div>
-            Photo Main Page
+        <div className="photo-main">
+            <Banner title="Your awsome photos" backgroundUrl={Images.PINK_BG} />
+
+            <Container className="text-center">
+                <Link to="/photos/add">Add New Photo</Link>
+            </Container>
         </div>
     );
 }
